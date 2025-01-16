8e9c149d88856050a12b6c3c6054570d944927af
dardematheus <matheusdarde9@gmail.com>
2025-01-16 17:13:35 -0300

    versao final do gamers

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Fazer usuario conseguir inputar o nome de amigos
+let listaAmigos = [];
+const list = document.getElementById('listaAmigos');
+const sorteado = document.getElementById('resultado');
+
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    listaAmigos.push(amigo);
+    document.getElementById('amigo').value = '';
+    
+    list.innerHTML += `<li>${amigo}</li>`;
+}
+
+function sortearAmigo(){
+    let numero = parseInt(Math.random() * listaAmigos.length);
+    let amigoSorteado = listaAmigos[numero];
+    
+    list.innerHTML = '';
+    sorteado.innerHTML = `Amigo sorteado : ${amigoSorteado}`;
+
+    console.log(`amigo sorteado : ${amigoSorteado} ${numero}`);
+    
+}
